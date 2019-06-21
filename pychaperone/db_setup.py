2d1126826a7fb0577ed403e7239a1529efb38269
Brandon Bertelsen <beb@pop-os.localdomain>
2019-06-21 00:42:59 -0500

    Adjustments for dynamic db

--- pychaperone/db_setup.py
+++ pychaperone/db_setup.py
@@ -1,7 +1,29 @@
-from peewee import Model, TextField, DateField, BooleanField, SqliteDatabase
+from peewee import Model, TextField, DateField, BooleanField, SqliteDatabase, PostgresqlDatabase, DatabaseProxy
 from playhouse.sqlite_ext import JSONField
 
-class QueueCheck(Model):
+database_proxy = DatabaseProxy()
+
+# def db_setup(type='sqlite', user=None, password=None, host=None, port=None):
+#     if type == 'sqlite':
+#         db = SqliteDatabase('queuecheck.db')
+    
+#     if type == 'postgresql':
+#         db = PostgresqlDatabase(
+#             'queuecheck', 
+#             user=user,
+#             password=password,
+#             host=host,
+#             port=port
+#         )
+
+#     return(db)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database_proxy
+
+class QueueCheck(BaseModel):
     item = TextField(unique=True)
     function = TextField(default="")
     complete = BooleanField(default=True)
